c7259286ac1fd7a233e51f4ebc80f7aaa38286de
Cayonliow <cayon.1318.96@hotmail.com>
2016-11-20 15:34:02 +0800

    20/11

---
+++ DB.js
@@ -0,0 +1,13 @@
+var mysql=require('mysql');
+console.log(mysql);
+var connection 
+var mysql = require('mysql');
+console.log(mysql);
+var connection = mysql.createConnetion({
+  host: 'localhost',
+    user: 'wp2016_groupI',
+      password: '********',
+        database: 'wp2016_group'
+        });
+          
+          connection.connect();
